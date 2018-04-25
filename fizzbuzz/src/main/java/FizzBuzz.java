3a806f9aeb37ce5ebc2042951687417cc5d4995d
Corentin Bect <corentin.bect@gmail.com>
2018-04-25 10:31:49 +0200

    Exercice 1

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,4 +1,8 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
@@ -18,6 +22,33 @@ public class FizzBuzz {
 
 
     public List<String> fizzBuzz() {
-        return integerList.mapToObj(String::valueOf).collect(toList());
+        Predicate<Integer> multipleOfThreePredicate = item -> item % 3 == 0;
+        Predicate<Integer> multipleOfFivePredicate = item -> item % 5 == 0;
+        Predicate<Integer> multipleOfThreeAndFivePredicate = item -> item % 3 == 0 && item % 5 == 0;
+        Predicate<Integer> multipleOfSevenPredicate = item -> item % 7 == 0;
+        Predicate<Integer> universalAnswerPredicate = item -> item == 42;
+        Predicate<Integer> equalsZeroPredicate = item -> item == 0;
+
+        List<String> fizzBuzzed = new ArrayList<>();
+        integerList.forEach(item -> {
+            String value = String.valueOf(item);
+
+            if(multipleOfSevenPredicate.test(item))
+                value = "It's a trap";
+            if(multipleOfFivePredicate.test(item))
+                value = "Buzz";
+            if(multipleOfThreeAndFivePredicate.test(item))
+                value = "Fizz Buzz";
+            if(multipleOfThreePredicate.test(item))
+                value = "Fizz";
+            if(universalAnswerPredicate.test(item))
+                value = "La grande question sur la vie, l'univers et le reste";
+            if(equalsZeroPredicate.test(item))
+                value = "0";
+
+            fizzBuzzed.add(value);
+        });
+
+        return fizzBuzzed;
     }
 }
--- fizzbuzz/src/test/java/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/FizzBuzzTest.java
@@ -3,7 +3,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Fizz Buzz Tests 😇")
@@ -24,5 +28,19 @@ class FizzBuzzTest {
     @DisplayName("should verify length positiv sign")
     void testNegativeLength() {
         assertThrows(AssertionError.class, () -> main = new FizzBuzz(-1));
+    }
+
+    @Test
+    @DisplayName("should show 'fizz' on multiples of 3")
+    void testRule2(){
+        main = new FizzBuzz(100);
+        List<String> fizzBuzzed = main.fizzBuzz();
+
+        System.out.println(fizzBuzzed.stream().map(String::toString).collect(Collectors.joining(", ")));
+
+
+        assertEquals("Fizz", fizzBuzzed.get(3));
+        assertEquals("Fizz", fizzBuzzed.get(6));
+        assertEquals("2", fizzBuzzed.get(2));
     }
 }
